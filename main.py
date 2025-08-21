193492920b4a9403e93c07b05b3716394a27105f
brytass <marvalradek@seznam.cz>
2025-08-21 23:42:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 """
 main.py: třetí projekt do Engeto Online Python Akademie
 
-author: Petr Svetr
-email: petr.svetr@gmail.com
+author: Radek Marval
+email: marvalradek@seznam.cz
 """
 
 import requests
